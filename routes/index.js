cd1a600fee9f1185952d3af71de6da1ff821f44b
Jeremy Dowell <jdowell@fossil.com>
2013-05-09 16:39:54 -0500

    thumbs

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@
 /*
  * GET home page.
  */
-var jsdom = require('jsdom')
-	, sys = require('sys')
-	, fs = require('fs')
-	, http = require('http')
-	, EventEmitter = require('events').EventEmitter
-	, radium = new EventEmitter();
 
 exports.index = function(req, res){	
 	  res.render('index', { title: 'album grabber' , albumName: null, message: null });
